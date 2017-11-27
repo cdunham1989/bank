757a1a39dd60ce0a7872574fc294a84114afc503
Chris <cdunham1989@gmail.com>
2017-11-27 12:09:14 +0000

    put in space to store transaction history within an array in the account class, unit test passed for this

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -3,8 +3,15 @@
 describe ('Account', function() {
   var account;
 
-  it('creates an account with the given balance', function() {
+  beforeEach(function() {
     account = new Account(1000);
+  });
+
+  it('creates an account with the given balance', function() {
     expect(account.getBalance()).toEqual(1000);
+  });
+
+  it ('can store the transaction history inside itself', function() {
+    expect(account.getTransactionHistory()).toEqual([]);
   });
 });
--- src/account.js
+++ src/account.js
@@ -1,13 +1,19 @@
 (function(exports) {
   exports.Account = function(balance) {
     var _balance = balance;
+    var _transactionHistory = [];
 
     getBalance = function() {
       return _balance;
     };
 
+    getTransactionHistory = function() {
+      return _transactionHistory;
+    };
+
     return {
-      getBalance: getBalance
+      getBalance: getBalance,
+      getTransactionHistory: getTransactionHistory
     };
   };
 })(this);
